fa420fd445ccf036238d443a502e78546feb63bb
matvey19-meet <matvey19@meet.mit.edu>
2017-11-29 19:25:13 +0200

    hii

---
+++ lab 2/shikma.py
@@ -0,0 +1,36 @@
+#1
+'''
+
+def shira():
+	shikma = ["a", "o", "j", "k", "p"]
+	hiii = [shikma[0], shikma[-1]]
+	return(hiii)
+
+print (shira())
+'''
+#2
+'''
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 2]
+def less_than_5 ():
+	for value in a:
+		if value < 5 :
+			print(value)
+
+less_than_5()
+'''
+'''
+#extrassss A
+
+a1 = [1, 1, 2, 3, 2]
+print (a1)
+'''
+
+#extrassss B
+a=[0, 47, 29,10,4,3,209,19]
+b=[]
+def user_input ():
+	for value in a:
+	person = input("plz write a number")
+		b.append (value > person)
+
+user_input()
--- lab 6/inheritense.py
+++ lab 6/inheritense.py
@@ -25,10 +25,52 @@ mainloop()
 """
 
 #Exercise Two:
-from turtle import Turtle
-turtle.registershape(hex ())
-class Hexagon(object):
-	def __init__ (self,size,color):
+from turtle import *
+
+'''
+begin_poly()
+def hexi():
+	for i in range (6):
+		pu()
+		hideturtle()
+		forward(25)
+		right(60)
+		speed(3000)
+
+hexi()
+end_poly()
+
+
+p = get_poly()
+register_shape("Hexagon", p)
+class Hexagon(Turtle):
+	def __init__ (self, size):
+		Turtle.__init__(self)
 		self.shapesize(size)
-		self.color(color)
-		self.shape()
+		self.color("violet")
+		self.shape("Hexagon")
+'''
+#hi = Hexagon(0.5)
+
+#Extras:
+from turtle import *
+class Poly(Turtle):
+	def __init__ (self,line):
+		Turtle.__init__(self)
+		
+		begin_poly()	
+		for i in range (line):
+			pu()
+			hideturtle()
+			speed(30000)
+			forward(50)
+			right (360/line)
+		end_poly()
+		r = get_poly()
+		register_shape("polygonz", r)
+		self.shape("polygonz")
+dan = Poly (7)
+
+mainloop()
+ #Exercise 3:
+ 
